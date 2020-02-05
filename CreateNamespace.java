6b0787927bfbd3a820cb6d1d62ca3db1b5722c24
TodoNintendoS <60708597+TodoNintendoS@users.noreply.github.com>
2020-02-05 18:20:54 +0100

    Update CreateNamespace.java

--- CreateNamespace.java
+++ CreateNamespace.java
@@ -8,13 +8,13 @@ public class CreateNamespace
 				new BufferedImage(ImagePrinter.width, 280, BufferedImage.TYPE_INT_ARGB);
 		
   		for(int i=0; i<ImagePrinter.width; i++)
-    	{
+    		{
   			for(int j=0; j<280; j++) 
   			{
   				int finalcolor = alpha<<24 | (red<<16) | (green<<8) | blue;
   				bgTemp.setRGB(i, j, finalcolor);			
   			}
-    	}
+    		}
   		
   		return bgTemp;
 	}
